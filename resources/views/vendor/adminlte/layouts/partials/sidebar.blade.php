90e446c10fc637f8bd59c2bccd15d0776f90afe8
Darinel Cruz <darinelcruzz@gmail.com>
2017-07-03 21:55:44 -0500

    No auth temporary for adding users

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -3,14 +3,18 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
         <!-- Sidebar Menu -->
-        @if (Auth::user()->level > 4)
-            @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/five')])
-        @elseif (Auth::user()->level > 3)
-            @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/four')])
-        @elseif (Auth::user()->level > 2)
-            @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/three')])
-        @elseif (Auth::user()->level > 1)
-            @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/two')])
+        @if (Auth::check())
+            @if (Auth::user()->level > 4)
+                @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/five')])
+            @elseif (Auth::user()->level > 3)
+                @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/four')])
+            @elseif (Auth::user()->level > 2)
+                @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/three')])
+            @elseif (Auth::user()->level > 1)
+                @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/two')])
+            @else
+                @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/one')])
+            @endif
         @else
             @include('adminlte::layouts.partials.menu_items', ['items' => trans('menus/one')])
         @endif
