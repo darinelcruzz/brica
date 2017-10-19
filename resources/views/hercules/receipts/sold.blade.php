d61b9d03e8203bc5a2e2c0005214f3c263e315c5
Darinel Cruz <darinelcruzz@gmail.com>
2017-10-19 10:27:44 -0500

    Hercules: sold receipts view fixed

--- resources/views/hercules/receipts/sold.blade.php
+++ resources/views/hercules/receipts/sold.blade.php
@@ -2,7 +2,7 @@
 
 @section('main-content')
 
-    <data-table col="col-md-12" title="Abonos"
+    <data-table col="col-md-12" title="Vendidas"
         example="example1" color="box-primary">
         <template slot="header">
             <tr>
@@ -16,29 +16,33 @@
 
         <template slot="body">
             @foreach($receipts as $receipt)
-                <tr>
-                    <td>{{ $receipt->id }}</td>
-                    <td>
-                        {{ $receipt->bodywork ? $receipt->bodyworkr->description: 'REPARACIÓN' }}
-                        &nbsp;&nbsp;&nbsp;
-                        <a href="{{ route('hercules.receipt.show', ['id' => $receipt->id]) }}"
-                            class="btn btn-primary btn-xs" title="IMPRIMIR RECIBO">
-                            <i class="fa fa-file-text" aria-hidden="true"></i>
-                        </a>
-                        &nbsp;
-                        @if ($receipt->order->photo)
-                            <a href="{{ Storage::url(substr($receipt->order->photo, 9)) }}"
-                              class="btn btn-primary btn-xs"  title='FOTO'>
-                              <i class="fa fa-eye" aria-hidden="true"></i>
-                            </a>
-                        @endif
-                    </td>
-                    <td>{{ '$ '. number_format($receipt->deposit, 2) }}</td>
-                    <td>{{ '$ '. number_format($receipt->amount, 2) }}</td>
-                    <td>
-                        {{ ucfirst($receipt->order->status) }}
-                    </td>
-                </tr>
+                @if ($receipt->order)
+                    @if ($receipt->order->status == 'pagado')
+                        <tr>
+                            <td>{{ $receipt->id }}</td>
+                            <td>
+                                {{ $receipt->bodywork ? $receipt->bodyworkr->description: 'REPARACIÓN' }}
+                                &nbsp;&nbsp;&nbsp;
+                                <a href="{{ route('hercules.receipt.show', ['id' => $receipt->id]) }}"
+                                    class="btn btn-primary btn-xs" title="IMPRIMIR RECIBO">
+                                    <i class="fa fa-file-text" aria-hidden="true"></i>
+                                </a>
+                                &nbsp;
+                                @if ($receipt->order->photo)
+                                    <a href="{{ Storage::url(substr($receipt->order->photo, 9)) }}"
+                                      class="btn btn-primary btn-xs"  title='FOTO'>
+                                      <i class="fa fa-eye" aria-hidden="true"></i>
+                                    </a>
+                                @endif
+                            </td>
+                            <td>{{ '$ '. number_format($receipt->deposit, 2) }}</td>
+                            <td>{{ '$ '. number_format($receipt->amount, 2) }}</td>
+                            <td>
+                                {{ ucfirst($receipt->order->status) }}
+                            </td>
+                        </tr>
+                    @endif
+                @endif
             @endforeach
         </template>
     </data-table>
