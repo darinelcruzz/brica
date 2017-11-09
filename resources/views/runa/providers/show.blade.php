d5c3a463734fac3bdce3cf3ac76a06597f77c3ca
Darinel <darinelcruzz@gmail.com>
2017-11-09 12:32:12 -0600

    Runa: color boxes added to providers

--- resources/views/runa/providers/show.blade.php
+++ resources/views/runa/providers/show.blade.php
@@ -3,7 +3,7 @@
 @section('main-content')
 
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-9">
             <data-table col="col-md-12" title="Lista de compras" example="example1" color="box-warning">
                 <template slot="header">
                     <tr>
@@ -11,35 +11,47 @@
                         <th>Producto</th>
                         <th>Precio</th>
                         <th>KG</th>
-                        <th>Total</th>
+                        <th>Importe</th>
+                        <th>Abonado</th>
                         <th>Estado</th>
                     </tr>
                 </template>
+
+                @php
+                    $tshops = $tdebt = $tpay = 0;
+                @endphp
 
                 <template slot="body">
                     @foreach ($rprovider->shoppings as $shopping)
                         <tr>
                             <td>{{ $shopping->shop_date }}</td>
                             <td>{{ $shopping->product }}</td>
-                            <td align="right">$ {{ number_format($shopping->unit_price, 2) }}</td>
+                            <td align="right">{{ number_format($shopping->unit_price, 2) }}</td>
                             <td align="right">{{ $shopping->kg }}</td>
-                            <td align="right">$ {{ number_format($shopping->kg * $shopping->unit_price, 2) }}</td>
+                            <td align="right">{{ number_format($shopping->amount, 2) }}</td>
+                            <td align="right">{{ number_format($shopping->amount - $shopping->pending, 2) }}</td>
                             <td>
                                 <code style="{{ $shopping->pending == 0 ? "color:#04b07b;": ''}}">
                                     {{ strtoupper($shopping->status) }}
                                 </code>&nbsp;
                                 <a href="{{ route('runa.provider.deposit', ['rshopping' => $shopping->id ]) }}"
-                                    class="btn btn-xs btn-success">
+                                    class="btn btn-xs btn-success" title="ABONAR">
                                     <i class="fa fa-usd" aria-hidden="true"></i>
                                 </a>
                             </td>
                         </tr>
+
+                        @php
+                            $tshops += $shopping->amount;
+                            $tdebt += $shopping->pending;
+                            $tpay +=  $shopping->amount - $shopping->pending;
+                        @endphp
                     @endforeach
                 </template>
             </data-table>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-3">
             <solid-box title="Agregar compra" color="box-warning">
                 {!! Form::open(['method' => 'POST', 'route' => 'runa.provider.shop']) !!}
                     {!! Field::date('date', Date::now(), ['tpl' => 'templates/withicon'], ['icon' => 'calendar-o']) !!}
@@ -59,6 +71,23 @@
                 {!! Form::close() !!}
             </solid-box>
         </div>
+    </div>
+
+    <div class="row">
+    	<little-box color="bg-green" size="col-md-4" icon="fa fa-shopping-cart">
+    		<p>Compras</p>
+    		<h3>{{ '$ ' . number_format($tshops, 2) }}</h3>
+    	</little-box>
+
+    	<little-box color="bg-red" size="col-md-4" icon="fa fa-credit-card">
+    		<p>Adeudos</p>
+    		<h3>{{ '$ ' . number_format($tdebt, 2) }}</h3>
+    	</little-box>
+
+    	<little-box color="bg-primary" size="col-md-4" icon="fa fa-usd">
+    		<p>Pagos</p>
+    		<h3>{{ '$ ' . number_format($tpay, 2) }}</h3>
+    	</little-box>
     </div>
 
 @endsection
--- routes/public.php
+++ routes/public.php
@@ -1,13 +1,5 @@
 <?php
 
-function usesas($ctrl, $fun, $as = null)
-{
-    if ($as) {
-        return ['uses' => "$ctrl@$fun", 'as' => $as];
-    }
-    return ['uses' => "$ctrl@$fun", 'as' => $fun];
-}
-
 Auth::routes();
 
 Route::get('salir', function ()
