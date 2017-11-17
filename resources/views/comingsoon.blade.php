ff687960b1bf66d9b46d65335ba5d25261b731ac
Darinel <darinelcruzz@gmail.com>
2017-11-16 18:28:51 -0600

    Schema.org test

--- resources/views/comingsoon.blade.php
+++ resources/views/comingsoon.blade.php
@@ -16,5 +16,34 @@
   </div>
 
 </div>
+
+<div itemscope itemtype="http://schema.org/MedicalWebPage">
+  <link itemprop="audience" href="http://schema.org/Patient" />
+  <link itemprop="specialty" href="http://schema.org/Cardiovascular" />
+  <meta itemprop="lastReviewed" content="2017-09-14"/>
+  <h1>Acerca
+    <span itemprop="about" itemscope itemtype="http://schema.org/MedicalCondition">
+      <span itemprop="name">Presión alta</span>
+      (<span itemprop="name">Hipertensión</span>)</span>
+  </h1>
+  ...
+  <h2><span itemprop="aspect">Diagnosis</span></h2>
+  High blood pressure is diagnosed by measuring ...
+  ...
+  <h2><span itemprop="aspect">Treatment</span></h2>
+  There are many common treatments for high blood pressure,
+  including
+  <span itemscope itemtype="http://schema.org/DrugClass">
+    <span itemprop="name">beta-blocker</span> drugs such as
+    <span itemprop="drug" itemscope itemtype="http://schema.org/Drug">
+      <span itemprop="nonProprietaryName">propanaolol</span>
+      (<span itemprop="otherName">Innopran</span>)
+    </span>
+  and
+    <span itemprop="drug" itemscope itemtype="http://schema.org/Drug">
+      <span itemprop="nonProprietaryName">atenolol</span>
+      (<span itemprop="otherName">Tenormin</span>)
+    </span>
+</div>
 </body>
 </html>
