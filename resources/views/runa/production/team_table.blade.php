90802ae7a872f95b4845cd1c378c25f51dd5f9ae
Darinel Cruz Vázquez <darinelcruzz@gmail.com>
2018-05-04 12:33:27 -0500

    Runa: productivity page with sales since april 30th

---
+++ resources/views/runa/production/team_table.blade.php
@@ -0,0 +1,53 @@
+<data-table col="col-md-12" title="{{ $team['title'] }}"
+    example="example{{ $team['example'] }}" color="box-{{ $team['color'] }}" collapsed="collapsed-box">
+
+    <template slot="header">
+        <tr>
+            <th width="5%">#</th>
+            <th width="20%">Cliente</th>
+            <th width="25%">Descripción</th>
+            <th width="10%">Detalles</th>
+            <th width="10%">Equipo</th>
+            <th width="20%"><i class="fa fa-clock-o"></i></th>
+            <th width="10%">Peso</th>
+        </tr>
+    </template>
+
+    <template slot="body">
+      @foreach($terminated->where('team', 'R1')->where('weight', 0) as $row)
+        @if($row->updated_at > '2018-04-30')
+          <tr>
+              <td>{{ $row->folio }}</td>
+              <td>{{ $row->clientr->name }}</td>
+              <td>{{ $row->description }}</td>
+              <td>
+                  <a href="{{ route('runa.quotation.details', ['id' => $row->id]) }}" class="btn btn-{{ $team['color'] }} btn-xs">
+                      <i class="fa fa-info" aria-hidden="true"></i>nfo
+                      <i class="fa fa-forward" aria-hidden="true"></i>
+                  </a>
+              </td>
+              <td>{{ $row->team }}</td>
+              <td>
+                  {{ $row->startTime }} <br>
+                  {{ $row->endTime }}
+              </td>
+              <td>
+                {!! Form::open(['method' => 'POST', 'route' => 'runa.manager.addWeight']) !!}
+
+                  <div class="input-group input-group-sm">
+                    <input type="hidden" name="id" value="{{ $row->id }}">
+                    <input type="number" class="form-control" name="weight" min="0" value="0" step="0.01" required>
+                    <span class="input-group-btn">
+                      <button type="submit" class="btn btn-{{ $team['color'] }} btn-flat btn-xs">
+                      <i class="fa fa-plus"></i>
+                      </button>
+                    </span>
+                  </div>
+
+                {!! Form::close() !!}
+              </td>
+          </tr>
+        @endif
+      @endforeach
+    </template>
+</data-table>
