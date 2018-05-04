edb56ce7e616d66ff6f1fd93dc5f4598939ac1b6
Darinel Cruz Vázquez <darinelcruzz@gmail.com>
2018-05-03 19:58:50 -0500

    Results page modified!

--- resources/views/runa/production/results.blade.php
+++ resources/views/runa/production/results.blade.php
@@ -4,30 +4,30 @@
 
   <div class="row">
     <little-box color="bg-green" size="col-md-4" icon="fa fa-balance-scale">
-      <p>KG</p>
-      <h3>RUNA 1: {{ count($terminated->where('team', 'RC')->where('weight', '>', 0)) }}</h3>
+      <p></p>
+      <h3>R1</h3><h4>{{ $terminated->where('team', 'RC')->where('weight', '>', 0)->sum('weight') }} kg</h4>
     </little-box>
 
     <little-box color="bg-red" size="col-md-4" icon="fa fa-balance-scale">
-      <p>KG</p>
-      <h3>RUNA 2: {{ count($terminated->where('team', 'R2')->where('weight', '>', 0)) }}</h3>
+      <p></p>
+      <h3>R2</h3><h4>{{ $terminated->where('team', 'R2')->where('weight', '>', 0)->sum('weight') }} kg</h4>
     </little-box>
 
     <little-box color="bg-primary" size="col-md-4" icon="fa fa-balance-scale">
-      <p>KG</p>
-      <h3>RUNA 3: {{ count($terminated->where('team', 'R3')->where('weight', '>', 0)) }}</h3>
+      <p></p>
+      <h3>R3</h3><h4>{{ $terminated->where('team', 'R3')->where('weight', '>', 0)->sum('weight') }} kg</h4>
     </little-box>
   </div>
 
   <div class="row">
     <little-box color="bg-yellow" size="col-md-4" icon="fa fa-balance-scale">
-      <p>KG</p>
-      <h3>RUNA 4: {{ count($terminated->where('team', 'R4')->where('weight', '>', 0)) }}</h3>
+      <p></p>
+      <h3>R4</h3><h4>{{ $terminated->where('team', 'R4')->where('weight', '>', 0)->sum('weight') }} kg</h4>
     </little-box>
 
     <little-box color="bg-purple" size="col-md-4" icon="fa fa-balance-scale">
-      <p>KG</p>
-      <h3>RUNA C: {{ count($terminated->where('team', 'RC')->where('weight', '>', 0)) }}</h3>
+      <p></p>
+      <h3>RC</h3><h4>{{ $terminated->where('team', 'RC')->where('weight', '>', 0)->sum('weight') }} kg</h4>
     </little-box>
   </div>
 
