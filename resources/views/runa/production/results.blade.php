9ea49dcdde8c229b87325a640c453deb21ad954b
Darinel Cruz Vázquez <darinelcruzz@gmail.com>
2018-05-04 10:52:58 -0500

    Hercules: results page sum fixed!

--- resources/views/runa/production/results.blade.php
+++ resources/views/runa/production/results.blade.php
@@ -5,29 +5,29 @@
   <div class="row">
     <little-box color="bg-green" size="col-md-4" icon="fa fa-balance-scale">
       <p></p>
-      <h3>R1</h3><h4>{{ $terminated->where('team', 'RC')->where('weight', '>', 0)->sum('weight') }} kg</h4>
+      <h3>R1</h3><h4>{{ $terminated->where('team', 'R1')->sum('weight') }} kg</h4>
     </little-box>
 
     <little-box color="bg-red" size="col-md-4" icon="fa fa-balance-scale">
       <p></p>
-      <h3>R2</h3><h4>{{ $terminated->where('team', 'R2')->where('weight', '>', 0)->sum('weight') }} kg</h4>
+      <h3>R2</h3><h4>{{ $terminated->where('team', 'R2')->sum('weight') }} kg</h4>
     </little-box>
 
     <little-box color="bg-primary" size="col-md-4" icon="fa fa-balance-scale">
       <p></p>
-      <h3>R3</h3><h4>{{ $terminated->where('team', 'R3')->where('weight', '>', 0)->sum('weight') }} kg</h4>
+      <h3>R3</h3><h4>{{ $terminated->where('team', 'R3')->sum('weight') }} kg</h4>
     </little-box>
   </div>
 
   <div class="row">
     <little-box color="bg-yellow" size="col-md-4" icon="fa fa-balance-scale">
       <p></p>
-      <h3>R4</h3><h4>{{ $terminated->where('team', 'R4')->where('weight', '>', 0)->sum('weight') }} kg</h4>
+      <h3>R4</h3><h4>{{ $terminated->where('team', 'R4')->sum('weight') }} kg</h4>
     </little-box>
 
     <little-box color="bg-purple" size="col-md-4" icon="fa fa-balance-scale">
       <p></p>
-      <h3>RC</h3><h4>{{ $terminated->where('team', 'RC')->where('weight', '>', 0)->sum('weight') }} kg</h4>
+      <h3>RC</h3><h4>{{ $terminated->where('team', 'RC')->sum('weight') }} kg</h4>
     </little-box>
   </div>
 
