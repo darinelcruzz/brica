badee6814fc51793c9ad0d1eccf905da1d2c22e2
Darinel Cruz Vázquez <darinelcruzz@gmail.com>
2018-09-27 18:21:33 -0500

    Birthday home view added

--- resources/views/hercules/home.blade.php
+++ resources/views/hercules/home.blade.php
@@ -1,7 +1,11 @@
 @extends('hercules')
 
 @section('main-content')
-    <h3>Bienvenido, {{ $user->name or '' }}</h3>
+    @if (date('d-m') == '27-09' && auth()->user()->id == 10)
+    	<h3>¡¡Feliz cumpleaños, Fernando!!</h3>
+    @else
+    	<h3>Bienvenido, {{ $user->name or '' }}</h3>
+    @endif
     <div align="center">
         <img width="60%" height="60%" src="{{ asset('/img/carrocerias.png') }}">
     </div>
--- resources/views/runa/home.blade.php
+++ resources/views/runa/home.blade.php
@@ -1,8 +1,15 @@
 @extends('runa')
 
 @section('main-content')
-    <h3>Bienvenido, {{ $user->name or '' }}</h3>
-    <div align="center">
-    	<img width="50%" height="50%" src="{{ asset('/img/logoruna.png') }}">
-    </div>
+    @if (date('d-m') == '27-09' && auth()->user()->id == 10)
+    	<h3>¡¡Feliz cumpleaños, Fernando!!</h3>
+    	<div align="center">
+	    	<img width="50%" height="50%" src="{{ asset('/img/cumple.png') }}">
+	    </div>
+    @else
+    	<h3>Bienvenido, {{ $user->name or '' }}</h3>
+    	 <div align="center">
+	    	<img width="50%" height="50%" src="{{ asset('/img/logoruna.png') }}">
+	    </div>
+    @endif
 @endsection
